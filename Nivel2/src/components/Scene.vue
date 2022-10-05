15755f76fb641bcbf87447d3591a4305c30aefdc
aobjsvue <andres.orta@hotmail.com>
2022-10-05 11:50:01 +0200

    nivel2uploaded

---
+++ Nivel2/src/components/Scene.vue
@@ -0,0 +1,24 @@
+<script>
+  export default {
+    name: "Scene",
+    props: ["arrayText", "currentSentence"]
+  }
+</script>
+
+<template>
+  <div>
+    <p v-for="(text, index) in arrayText" :key="index" :class="{active: index === currentSentence}">{{ text.story }}</p>
+  </div>
+</template>
+
+<style>
+  p {
+    text-align: center;
+    border: 2px solid black;
+    border-radius: 1.8em;
+    padding: 1em 0;
+  }
+  .active {
+    background-color: lightpink;
+  }
+</style>
